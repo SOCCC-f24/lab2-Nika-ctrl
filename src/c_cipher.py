83b857afbc9f2b9a951fa8fd6decaf159e67cc34
Nika-ctrl <ns139@sunyorange.edu>
2024-09-26 21:22:24 -0400

    Update c_cipher.py
    i updated and crept

--- src/c_cipher.py
+++ src/c_cipher.py
@@ -1,54 +1,55 @@
 import logging
+
 logging.basicConfig(level=logging.INFO, format='%(message)s')
 
 def encrypt(email="abc012"):
     """
-    TODO: What is the objective? 
+    Encrypts the email string by shifting each character's ASCII value up by 3.
 
     Args:
-        TODO: what arguments and data types are expected? (i.e., email)
+        email (str): The email string to be encrypted.
 
     Returns:
-        TODO: what varibale and data types are being returned?   
+        str: The encrypted email string.
     """
-    output = "" 
+    output = ""
     len_flag = len(email) != 6
-    # TODO: fix line below and, implement functionality rather than literals
-    # keep all updates in the anum_flag (bool) variable
-    # i.e., 
-    #     A = email[:3] (check first half)
-    #     B = email[3:] (check second half)
-    #     enum_flag = A or B
-    anum_flag = email[:3] != 'abc' or email[3:] != '012' 
+    anum_flag = email[:3] != 'abc' or not email[3:].isdigit()
 
-    if len_flag:                         # NOTE: here we provide input validation on length
+    if len_flag:
         output = "Length check failed\n"
         output += "Email must be 6 characters long."
         logging.info(output)
-        return output        
-    if anum_flag:                        # NOTE: here we provide input validation on alpha/num
-        output = "alpha num check failed\n"
+        return output
+
+    if anum_flag:
+        output = "Alphanumeric check failed\n"
         output += "Email must have 3 letters followed by 3 digits."
         logging.info(output)
-        return output     
-        
-    # TODO: fix line below, process our string into a list
-    email_lst = ["a", "b", "c", "0", "1", "2"]
-        
-    # TODO: complete line(s) below, convert EACH new element into a string
-    new_ascii = ord(email_lst[0]) + 3    # NOTE: here we extract and update element at 0 
-    email_lst[0] = chr(new_ascii)        # NOTE: here we convert our ASCII into string
-        
-    # TODO: fix line below, convert list into a string
-    email_str = "dbc012"
+        return output
 
-    # keep all updates in the retVal (str) variablei
-    # i.e.,
-    #    email_str = " some string updates here "
-    #    email_1 = email_str.strip()
-    #    retVal = email_1
-    retVal = email_str
-    return retVal 
+    # Convert email string to list to modify individual characters
+    email_lst = list(email)
+
+    # Encrypt each character by shifting its ASCII value up by 3
+    for i in range(len(email_lst)):
+        char = email_lst[i]
+        new_ascii = ord(char) + 3
+        if char.islower():
+            if new_ascii > ord('z'):
+                new_ascii -= 26
+        elif char.isupper():
+            if new_ascii > ord('Z'):
+                new_ascii -= 26
+        email_lst[i] = chr(new_ascii)
+
+    # Convert list back to string
+    encrypted_email = ''.join(email_lst)
+    return encrypted_email
+
+# Testing the function
+encrypted_result = encrypt("abc012")
+print("Encrypted result:", encrypted_result)l
 
 def decrypt(email="def345"):
     """
